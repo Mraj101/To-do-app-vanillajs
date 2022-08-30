568b224db485cedee1fa9099aa5032fb20e61cd6
Hasnain <mrajhasnain101@gmail.com>
2022-08-30 21:30:11 +0600

    main js updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-import { productdb } from './Module.js'
-import {sum} from './Module.js'
+import productdb,{
+  bulkcreate
+} from './Module.js'
+
 productdb("productdb", {
   products: `++id,name,seller,price`,
 });
 
-console.log(sum());
+
+//input tags
+const userid=document.getElementById('userid');
+const proname=document.getElementById('productname');
+const seller=document.getElementById('seller');
+const price=document.getElementById('price');
+
+//btn create
+const btncreate=document.getElementById('btn-create');
+const btnread=document.getElementById('btn-read');
+const btnupdate=document.getElementById('btn-update');
+const btndelete=document.getElementById('btn-delete');
+
+//insert value using create button
+btncreate.onclick=(event)=>{
+  let flag=bulkcreate(productdb.products,{
+    name:proname.value,
+    seller:seller.value,
+    price:price.value,
+  })
+  console.log(flag);
+}
